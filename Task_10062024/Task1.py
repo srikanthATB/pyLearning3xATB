baec4d202e4f42295c8269ba32ddcb00081f6262
srikanthATB <1996srikanthb@gmail.com>
2024-06-13 06:47:45 +0530

    Task : 10062024 changes and Ex_12062024 files added

--- Task_10062024/Task1.py
+++ Task_10062024/Task1.py
@@ -12,5 +12,24 @@
 
 
 # 3.What does the ^ operator do in Python, and in what context is it commonly used?
-# '^' operator also used to find square of number (ex: 3^2 it will output as : 3*3=9)
+# """
+# The ^ operator compares each bit and set it to 1 if only one is 1, otherwise (if both are 1 or both are 0) it is set to 0:
+#
+# 6 = 0000000000000110
+# 3 = 0000000000000011
+# --------------------
+# 5 = 0000000000000101
+# ====================
+#
+# Decimal numbers and their binary values:
+# 0 = 0000000000000000
+# 1 = 0000000000000001
+# 2 = 0000000000000010
+# 3 = 0000000000000011
+# 4 = 0000000000000100
+# 5 = 0000000000000101
+# 6 = 0000000000000110
+# 7 = 0000000000000111
+# """
+print(6^3)
 
